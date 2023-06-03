56345409d572d7b693471bbf2d713a7bcdf26668
zcxxxxxxx <2049314799@qq.com>
2023-06-03 19:49:32 +0800

    完成1.1.2

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -12,6 +12,11 @@ import {
   TeaSet2,
 } from "../../../docs/frontend/React/components/Describ_1.js";
 
+import {
+  Profile1_2_1,
+  Gallery1_2_1,
+} from "../../../docs/frontend/React/components/Describ_2.js";
+
 const ReactLiveScope = {
   React,
   ...React,
@@ -24,6 +29,8 @@ const ReactLiveScope = {
   List1,
   TeaSet1,
   TeaSet2,
+  Profile1_2_1,
+  Gallery1_2_1,
 };
 
 export default ReactLiveScope;
