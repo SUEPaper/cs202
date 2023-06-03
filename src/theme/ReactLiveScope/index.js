fa23c3f6211f2fdbbb07ca1ba85ae793236f1dd6
zcxxxxxxx <2049314799@qq.com>
2023-06-03 19:22:01 +0800

    完成1.1

---
+++ src/theme/ReactLiveScope/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+// Add react-live imports you need here
+import {
+  Gallery1,
+  Gallery2,
+  TodoList1,
+  TodoList2,
+  Profile3,
+  PackingList1,
+  List1,
+  TeaSet1,
+  TeaSet2,
+} from "../../../docs/frontend/React/components/Describ_1.js";
+
+const ReactLiveScope = {
+  React,
+  ...React,
+  Gallery1,
+  Gallery2,
+  TodoList1,
+  TodoList2,
+  Profile3,
+  PackingList1,
+  List1,
+  TeaSet1,
+  TeaSet2,
+};
+
+export default ReactLiveScope;
