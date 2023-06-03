1fb441557c5d80e510bdd443367f2fe99bcf0ad3
Mright-Red <3112069360@qq.com>
2023-06-04 02:20:00 +0800

    完成了React第三部分的序章

---
+++ docs/frontend/React/components/manage/Addtask.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+export  function AddTask({ onAddTask }) {
+  const [text, setText] = useState('');
+  return (
+    <>
+      <input
+        placeholder="添加任务"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <button onClick={() => {
+        setText('');
+        onAddTask(text);
+      }}>添加</button>
+    </>
+  )
+}
--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -17,6 +17,18 @@ import {
   Gallery1_1_1,
 } from "../../../docs/frontend/React/components/Describ_1.js";
 
+import {
+  Form3_0_1,
+  Form3_0_2,
+  Form3_0_3,
+  Accordion3_0_4,
+  Messenger3_0_5,
+  Messenger3_0_6,
+  TaskApp3_0_7,
+  Page3_0_8,
+  TaskApp3_0_9,
+} from "../../../docs/frontend/React/components/manage/manage_0.js";
+
 const ReactLiveScope = {
   React,
   ...React,
@@ -31,6 +43,15 @@ const ReactLiveScope = {
   TeaSet2,
   Profile1_1_1,
   Gallery1_1_1,
+  Form3_0_1,
+  Form3_0_2,
+  Form3_0_3,
+  Accordion3_0_4,
+  Messenger3_0_5,
+  Messenger3_0_6,
+  TaskApp3_0_7,
+  Page3_0_8,
+  TaskApp3_0_9
 };
 
 export default ReactLiveScope;
