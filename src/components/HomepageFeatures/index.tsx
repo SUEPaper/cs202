3ef535645b08c38420ce9d15aeaa5aa25f3d3926
HaiwenZhang <haiwensummer@gmail.com>
2023-06-10 20:27:13 +0800

    Add Course Introduction

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react";
 
+import CourseInfos from "../CourseInfos";
+import Schedules from "../Schedules";
+import Teachers from "../Teachers";
+import TAs from "../TAs";
+import Maintainers from "../Maintainers";
 
-import CourseInfos from '../CourseInfos';
-import Schedules from '../Schedules';
-import Teachers from '../Teachers';
-import TAs from '../TAs';
-import Maintainers from '../Maintainers';
-
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 export default function HomepageFeatures(): JSX.Element {
   return (
-      <div className="container w-5/6 mt-14 mx-auto">
-          <CourseInfos />
-          <Schedules />
-          <Teachers />
-          <TAs />
-          <Maintainers />
-      </div>
+    <div className="container w-5/6 mt-14 mx-auto">
+      <CourseInfos />
+      {/* <Schedules /> */}
+      <Teachers />
+      <TAs />
+      <Maintainers />
+    </div>
   );
 }
